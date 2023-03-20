f404d69fa3a2fcfd2da0337236869836885b6d0e
Jordan_Walls-Wilson <jmww1998@gmail.com>
2023-03-19 21:51:31 -0700

    School work first push

---
+++ models/customer.js
@@ -0,0 +1,99 @@
+/** Customer for Lunchly */
+
+const db = require("../db");
+const Reservation = require("./reservation");
+
+/** Customer of the restaurant. */
+
+class Customer {
+  constructor({ id, firstName, lastName, phone, notes }) {
+    this.id = id;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.phone = phone;
+    this.notes = notes;
+    
+  }
+
+  /** find all customers. */
+
+  static async all() {
+    const results = await db.query(
+      `SELECT id, 
+         first_name AS "firstName",  
+         last_name AS "lastName", 
+         phone, 
+         notes
+       FROM customers
+       ORDER BY last_name, first_name`
+    );
+    return results.rows.map(c => new Customer(c));
+  }
+
+// ** this only wokrs on the customer detail page. I pushed it around a few times but was not able to get to work on the customer list page. **
+
+  static async fullName(id) {
+    const firstName = await db.query(
+    `SELECT id, first_name AS "firstName" 
+    FROM customers WHERE id = $1`, [id]);
+    const lastName = await db.query(
+      `SELECT last_name AS "lastName"  
+    FROM customers WHERE id = $1`, [id]
+    );
+    const fullName = `${firstName.rows[0].firstName} ${lastName.rows[0].lastName}`;
+
+    return fullName;
+  }
+
+  /** get a customer by ID. */
+
+  static async get(id) {
+    const results = await db.query(
+      `SELECT id, 
+         first_name AS "firstName",  
+         last_name AS "lastName", 
+         phone, 
+         notes 
+        FROM customers WHERE id = $1`,
+      [id]
+    );
+
+    const customer = results.rows[0];
+
+    if (customer === undefined) {
+      const err = new Error(`No such customer: ${id}`);
+      err.status = 404;
+      throw err;
+    }
+
+    return new Customer(customer);
+  }
+
+  /** get all reservations for this customer. */
+
+  async getReservations() {
+    return await Reservation.getReservationsForCustomer(this.id);
+  }
+
+  /** save this customer. */
+
+  async save() {
+    if (this.id === undefined) {
+      const result = await db.query(
+        `INSERT INTO customers (first_name, last_name, phone, notes)
+             VALUES ($1, $2, $3, $4)
+             RETURNING id`,
+        [this.firstName, this.lastName, this.phone, this.notes]
+      );
+      this.id = result.rows[0].id;
+    } else {
+      await db.query(
+        `UPDATE customers SET first_name=$1, last_name=$2, phone=$3, notes=$4
+             WHERE id=$5`,
+        [this.firstName, this.lastName, this.phone, this.notes, this.id]
+      );
+    }
+  }
+}
+
+module.exports = Customer;
